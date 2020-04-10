8e1d7c2e6be04db72b8583a42173f00a88c98c3b
Ramon Gabriel Pichardo Albert <el.gabi96@hotmail.com>
2020-04-09 20:24:38 -0400

    no message

---
+++ views/ofertaAcademica.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+	    <link href="../css/bootstrap.min.css" rel="stylesheet" />
+		<link href="../css/style.css" rel="stylesheet" />
+ 		<link rel="stylesheet" href="../css/uikit.min.css"/>
+		<link href="../css/index.css" rel="stylesheet">
+	</head>
+	<body>
+    <?php  
+	    include ("navbar.php");
+    ?>
+		
+	</body>
+</html>
