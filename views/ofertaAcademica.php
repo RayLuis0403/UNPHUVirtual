0300092f574e66b4cf60759929ae0f66f2f41532
Ramon Gabriel Pichardo Albert <el.gabi96@hotmail.com>
2020-04-14 00:20:20 -0400

    no message

--- views/ofertaAcademica.php
+++ views/ofertaAcademica.php
@@ -8,6 +8,89 @@
 	<head>
 	</head>
 	<body>
-		
+	<div class="uk-container">
+	<H3><font color="green">Facultad Ciencia de la Salud:</font></H3>
+		<div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Medicina</font></h3>
+						<p>El programa de Medicina consta de 16 períodos que pueden ser completados en 5 años y 4 meses incluyendo el período de Premédica, ya que los estudiantes pueden realizar tres períodos al año de 15 semanas cada uno. Antes de ingresar al programa de la Escuela de Medicina de la UNPHU, la mayoría de nuestros estudiantes internacionales ya han realizado los estudios de Premédica en sus países de procedencia. Los estudiantes dominicanos regularmente realizan los estudios de Premédica en nuestra institución.</p>
+						<a href="../ofertaAcademica/Medicina.pdf" download="Medicina.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Odontología</font></h3>
+						<p>La escuela de Odontología de UNPHU, se encarga de la formación de profesionales altamente capacitados en la salud bucal del individuo, y precisa para lograr esta formación de 13 períodos, equivalente a 4 años y 6 meses.</p>
+						<a href="../ofertaAcademica/Odontologia.pdf" download="Odontologia.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Farmacia</font></h3>
+						<p>El pensum del programa de Farmacia consta de 12 semestres para ser cursados durante 4 años, los cuales están organizados de la siguiente manera: Ciclo Básico de 5 semestres y un ciclo profesional de 7 semestres. Este programa contiene materias electivas, así como prácticas tuteladas realizadas en las futuras áreas de trabajo profesional y una pasantía con un mínimo de 560 horas en la Industria Farmacéutica Nacional, lo cual le permite al estudiante adquirir el desenvolvimiento y las destrezas necesarias para competir con éxito en el mercado laboral.</p>
+						<a href="../ofertaAcademica/Farmacia.pdf" download="Farmacia.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+		</did>
+	</div>
+	
+	<div class="uk-container">
+	<H3><font color="green">Facultad de Ciencias y Tecnología:</font></H3>
+		<div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Ingeniería Civil</font></h3>
+						<p>El ingeniero civil egresado de nuestra universidad es un profesional con la capacidad y los conocimientos necesarios para participar en la planeación, el diseño, la construcción, la supervisión y el mantenimiento de obras para el desarrollo urbano, industrial, habitacional y de la infraestructura que requiere la sociedad, considerando los aspectos metodológicos, social, económico, técnico y ecológico, buscando el aprovechamiento óptimo de los recursos existentes en la naturaleza, bajo una perspectiva ética y humanística.</p>
+						<a href="../ofertaAcademica/Civil.pdf" download="Civil.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Ingeniería Química</font></h3>
+						<p>La vida entera es un proceso químico, la respiración, la digestión y hasta la propia atracción de los seres humanos es un proceso químico; no existe un área de la vida moderna en la que el ingeniero químico no haya aportado, es la ingeniería más completa, abarca los conocimientos de diseño de procesos y equipos, con habilidades para concebir, calcular, construir, poner en marcha, operar, evaluar, planificar, optimizar, dirigir, formar, liderar, auditar, prever cambios e innovar, en la industria y en los procesos en los que la materia cambia de forma, composición o energía; realizando una administración y optimización de los recursos.</p>
+						<a href="../ofertaAcademica/Quimica.pdf" download="Química.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="uk-card uk-card-default">
+					<div class="uk-card-media-top">
+						<img src="images/light.jpg" alt="">
+					</div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title"><font color="green">Carrera de Ingeniería Geomática</font></h3>
+						<p>La geomática o ingeniería geomática (término este último compuesto por geo, “Tierra”, y mática, “informática”), también llamada información espacial, información geoespacial e incluso tecnología geoespacial, es la ciencia que se ocupa de la gestión de información geográfica mediante la utilización de tecnologías de la información y la comunicación. La gestión incluye la adquisición, modelado, tratamiento, almacenamiento, recuperación, análisis, explotación, representación y difusión de la geodesia, la fotogrametría y teledetección, Sistemas de Información Geográfica e Infraestructura de Datos Espaciales, y está relacionada con cualquier ciencia que suponga el procesamiento de información geográfica.</p>
+						<a href="../ofertaAcademica/Geomatica.pdf" download="Geomatica.pdf">PDF</a>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+	
 	</body>
 </html>
